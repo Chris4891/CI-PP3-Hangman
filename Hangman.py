306dc1200746c887fd0fce6bc2aa41985ecd892c
Chris4891 <109537705+Chris4891@users.noreply.github.com>
2023-06-23 00:18:48 +0500

    setting the title for the window form

--- Hangman.py
+++ Hangman.py
@@ -6,3 +6,8 @@ def load_words_from_txt_file():
     with open('words.txt', 'r') as file:
         words = file.read().splitlines()
     return words    
+
+
+# GUI
+window = tk.Tk()
+window.title("Hangman Game")
