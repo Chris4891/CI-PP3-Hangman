6fb6c052639dbf90a666bf6b902241250f06b415
Chris4891 <109537705+Chris4891@users.noreply.github.com>
2023-06-23 00:14:47 +0500

    reading words from the text file

--- Hangman.py
+++ Hangman.py
@@ -1,3 +1,8 @@
 import tkinter as tk
 import random
 import math
+
+def load_words_from_txt_file():
+    with open('words.txt', 'r') as file:
+        words = file.read().splitlines()
+    return words    
