79cbb412d4767fe2b7b9d948ca181d5d7091d28e
Noman Ahmed Khan <109537705+Chris4891@users.noreply.github.com>
2023-06-23 17:30:53 +0500

    funtion to initialize the game  setting variables to be used throughout the game

--- Hangman.py
+++ Hangman.py
@@ -2,6 +2,8 @@ import random
 import colorama
 from colorama import Fore, Style
 
+colorama.init()
+
 def load_words_from_txt_file():
     with open('words.txt', 'r') as file:
         words = file.read().splitlines()
@@ -10,6 +12,9 @@ def load_words_from_txt_file():
 
 
 
-
-   
+def initialize_game(words):
+    global guess_word, remaining_attempts, guessed_letters
+    guess_word = random.choice(words)
+    remaining_attempts = 6
+    guessed_letters = set()
 
