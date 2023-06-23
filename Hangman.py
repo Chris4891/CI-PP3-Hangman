8eed009d898279c6453077837ad5de3d4d4e9b37
Noman Ahmed Khan <109537705+Chris4891@users.noreply.github.com>
2023-06-23 17:25:30 +0500

    Removing GUI as it is not supported by HEROKU

--- Hangman.py
+++ Hangman.py
@@ -1,4 +1,3 @@
-import tkinter as tk
 import random
 import math
 
@@ -95,39 +94,3 @@ def restart_game():
     message_label.config(text="")
 
 
-# GUI
-window = tk.Tk()
-window.title("Hangman Game")
-
-words = load_words_from_txt_file()
-
-window.configure(bg="white")
-
-username_label = tk.Label(window, text="Enter your name:", font=('Arial', 18), bg="white")
-username_label.pack(pady=20)
-
-username_entry = tk.Entry(window, font=('Arial', 16))
-username_entry.pack()
-
-start_button = tk.Button(window, text="Start Game", command=start_game, font=('Arial', 16), bg="white")
-start_button.pack(pady=20)
-
-hangman_image = tk.PhotoImage(file="img/hangman0.png")
-hangman_label = tk.Label(window, image=hangman_image, bg="white")
-
-word_label = tk.Label(window, font=('Arial', 24), bg="white")
-guess_label = tk.Label(window, text="Guess a letter:", font=('Arial', 18), bg="white")
-guess_entry = tk.Entry(window, font=('Arial', 24))
-guess_button = tk.Button(window, text="Guess", command=process_guess, font=('Arial', 16), bg="white")
-message_label = tk.Label(window, font=('Arial', 18), bg="white")
-
-screen_width = window.winfo_screenwidth()
-screen_height = window.winfo_screenheight()
-window_width = math.floor(screen_width * 0.8)
-window_height = math.floor(screen_height * 0.8)
-window_x = math.floor((screen_width - window_width) / 2)
-window_y = math.floor((screen_height - window_height) / 2)
-
-window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
-
-window.mainloop()
