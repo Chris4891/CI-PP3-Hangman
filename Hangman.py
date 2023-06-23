46c6b0eb1d75fda51d06b080c2c7bc9ba4232fa1
Noman Ahmed Khan <109537705+Chris4891@users.noreply.github.com>
2023-06-23 17:31:47 +0500

    adding ____ for the words to guess

--- Hangman.py
+++ Hangman.py
@@ -10,11 +10,13 @@ def load_words_from_txt_file():
     return words
 
 
-
-
 def initialize_game(words):
     global guess_word, remaining_attempts, guessed_letters
     guess_word = random.choice(words)
     remaining_attempts = 6
     guessed_letters = set()
 
+def update_word_display():
+    masked_word = ' '.join(
+        [letter if letter in guessed_letters else '_' for letter in guess_word])
+    print("Word:", Fore.YELLOW + masked_word + Style.RESET_ALL)
